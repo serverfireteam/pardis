56ff23f1db9ed70992ed9760adaff001b8ab2287
alenoosh <hope2end@gmail.com>
2013-12-30 12:56:54 +0330

    Some code cleanup is done.

--- views/footer.php
+++ views/footer.php
@@ -1,9 +1,3 @@
-			<footer class="row footer hbox full-width">
-	            <aside class="footer-copy-right">
-					<article><h6 class="bold-title">Copyright</h6></article>
-				</aside>
-                <aside class="footer-logo"><article></article></aside>
-            </footer>
 		</div>
 	</body>
 </html>
